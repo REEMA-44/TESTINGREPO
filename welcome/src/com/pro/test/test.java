7a84a54d8ea49945d99eb7297aa931c680a6caec
TL <TL@gmail.com>
2021-07-31 11:03:29 +0530

    created test.java by TL

---
+++ welcome/src/com/pro/test/test.java
@@ -0,0 +1,12 @@
+package com.pro.test;
+
+import com.pro.beans.utility;
+
+public class test {
+	
+	public static void main(string[] args) {
+		Utility u = new Utility();
+		System.out.println(U);
+	}
+
+}
